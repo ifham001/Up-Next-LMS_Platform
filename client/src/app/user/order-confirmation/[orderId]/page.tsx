d6055833897c501180fdca1e40db2a2b365ba186
ifham baig <ifhambaig@ifham-ka-MacBook-Air.local>
2025-08-23 13:43:09 +0530

    -v1.05

--- client/src/app/user/order-confirmation/[orderId]/page.tsx
+++ client/src/app/user/order-confirmation/[orderId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import OrderConfirmationPage from "@/component/order-confirmation/OrderConfirm";
+import ClientOnly from "@/util/CilentOnly";
 import { withAuth } from "@/util/withAuth";
 import React from "react";
 
@@ -10,7 +11,13 @@ type params = { orderId: string };
 async function page({ params }:{params:Promise<params>})  {
   const { orderId } = React.use(params); // ✅ unwrap the params Promise
 
-  return <OrderConfirmationPage orderId={orderId} />;
+  return (
+<ClientOnly> <OrderConfirmationPage orderId={orderId} /></ClientOnly>
+
+  )
+  
+  
+ 
 };
 
 export default withAuth(page);
